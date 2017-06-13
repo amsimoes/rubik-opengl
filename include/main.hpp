13cf1a65999740645e84848732a556c3c2f41d5c
António Simões <asimoes@student.dei.uc.pt>
2017-06-13 22:11:48 +0100

    adds main include, starts main cpp

---
+++ include/main.hpp
@@ -0,0 +1,15 @@
+#ifndef MAIN_HPP
+#define MAIN_HPP
+
+#ifdef __APPLE__
+	#include <OpenGl/glu.h>
+	#include <OpenGl/gl.h>
+#else
+	#include <GL/glew.h>
+#endif
+
+#include <GLFW/glfw3.h>
+
+using namespace std;
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include <math.h>
+#include <vector>
+#include <map>
+
+#define WINDOW_NAME "glRubik"
+
